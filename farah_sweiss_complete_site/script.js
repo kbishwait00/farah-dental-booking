ed8eef8d5803d3d77420c44bfc76db9fd5a7adc4
kbishwait00 <kbishwait@gmail.com>
2025-08-02 12:56:45 -0500

    Add files via upload

---
+++ farah_sweiss_complete_site/script.js
@@ -0,0 +1,6 @@
+document.getElementById("bookBtn").onclick = function() {
+  alert("Booking form will open.");
+};
+document.getElementById("cancelBtn").onclick = function() {
+  alert("Cancel form will open (if >24h before).");
+};
